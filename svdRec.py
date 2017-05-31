4fd38994b63ff3fb8cd1d49eb4614af4c9a0a514
zwmiller <zaglamir@gmail.com>
2017-05-31 18:05:53 -0500

    begin commenting the module

--- svdRec.py
+++ svdRec.py
@@ -10,6 +10,12 @@ from scipy.sparse.linalg import svds
 
 class svdRec():
     def __init__(self):
+        """
+        The svdRec class does nothing upon spawn, save for create a few 
+        variables that must exist for the class to work. The user must use
+        a seperate call to load data in, since the class accepts multiple data
+        types as input. 
+        """
         self.U, self.s, self.V = (None,None,None)
         self.user_encoder = None
         self.item_encoder = None
@@ -17,38 +23,94 @@ class svdRec():
         self.decomp = False
     
     def load_csv_sparse(self,filename,delimiter=',',skiprows=None):
+        """
+        Reads from a CSV file using numpy's 'loadtxt' function. The expected format for a row 
+        is: 
+        rowID, colID, Value, any, other, assorted, junk.
+        This defines the base sparse matrix (self.mat), and converts it from the coordinate type matrix
+        to a csr_matrix which is better for processing.
+        
+        Inputs: Filename, 
+        Kwargs: delmiter for the csv, how many rows to skip (if there's a header)
+        Returns: None
+        """
         print("Note: load_csv_sparse expects a csv in the format of: rowID, colID, Value, ...")
         u, m, r = np.loadtxt(filename, delimiter=delimiter, skiprows=skiprows, usecols=(0,1,2)).T
         self.mat = coo_matrix((r, (u-1, m-1)), shape=(u.max(), m.max())).tocsr()
         print("Created matrix of shape: ",self.mat.shape)
         
     def load_data_numpy(self, array, data_type=float):
+        """
+        Converts a numpy matrix or array (both are valid inputs) directly into a csr
+        sparse matrix for use with sparse SVD. This defines the base sparse matrix
+        (self.mat) for later user. 
+
+        Inputs: Filename, 
+        Kwargs: data_type to force the sparse matrix into float/int/etc
+        Returns: None
+        """
         self.mat = csr_matrix(array,dtype=data_type)
         print("Created matrix of shape: ",self.mat.shape)
         
     def load_item_encoder(self, d):
+        """
+        Takes in a dictionary of the format: {itemID: "item name"} to later be used
+        to decode from ID space to named space if necessary. If never called,
+        'get_item_name' returns a warning.
+
+        Inputs: Dictionary
+        Returns: None
+        """
         if type(d) != dict:
             raise TypeError("Encoder must be dictionary with key = itemID and value = Title")
         self.item_encoder = d
         
     def load_user_encoder(self, d):
+        """
+        Takes in a dictionary of the format: {userID: "user name"} to later be used
+        to decode from userID space to named space if necessary. If never called,
+        'get_user_name' returns a warning.
+
+        Inputs: Dictionary
+        Returns: None
+        """
         if type(d) != dict:
             raise TypeError("Encoder must be dictionary with key = userID and value = Title")
         self.user_encoder = d
         
     def get_item_name(self,itemid):
+        """ 
+        Returns the name of an item given its ID number
+
+        Input: Item id
+        Return: Name
+        """
         if self.item_encoder:
             return self.item_encoder[str(itemid)]
         else:
             return "No ItemId -> Item-name Encoder Built!"
     
     def get_user_name(self,userid):
+        """ 
+        Returns the name of a user given it's ID number
+
+        Input: User id
+        Return: Name
+        """
         if self.item_encoder:
             return self.user_encoder[str(userid)]
         else:
             return "No UserID -> Username Encoder Built!"
     
     def SVD(self, num_dim=None):
+        """
+        Computes the matrix decomposition for the data, setting U, VT, and s to the output matrices.
+        This is based on 'svds', a singular value decomposition library designed to work with
+        scipy sparse matrices.
+
+        Input: num_dim is the number of dimensions for truncating SVD. 
+        Returns: None
+        """
         if num_dim==None:
             print("Number of SVD dimensions not requested, using %s dimensions." % (min(self.mat.shape)-1), "To set, use num_dim.")
             num_dim = min(self.mat.shape)-1
@@ -56,6 +118,12 @@ class svdRec():
         self.decomp = True
     
     def get_cell(self,i,j):
+        """
+        Return value for a cell in the sparse matrix.
+
+        Inputs: row val, col val
+        Returns: value of cell
+        """
         return self.mat[1,:].toarray()[0,j]
     
     def get_similar_items(self, itemID, num_recom=5, show_similarity=False):
