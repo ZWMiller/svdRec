9c8072b7cbfe75e00401b03794b13d2e7fcb0c07
zwmiller <zaglamir@gmail.com>
2017-06-01 16:53:19 -0500

    Reorganize for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'svdRec',
+  packages = ['svdRec'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A Python3 Collaborative Recommendation Engine with Sparse Matrices',
+  author = 'Zachariah Miller',
+  author_email = 'zaglamir@gmail.com',
+  url = 'https://github.com/ZWMiller/svdRec', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
