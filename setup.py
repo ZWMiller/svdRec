08c30315f7c61ec17356ccc1d9a14127f4f1bed4
zwmiller <zaglamir@gmail.com>
2017-06-02 09:05:51 -0500

    Update for pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
   author = 'Zachariah Miller',
   author_email = 'zaglamir@gmail.com',
   url = 'https://github.com/ZWMiller/svdRec', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  download_url = 'https://github.com/zwmiller/svdRec/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['recommender', 'sparse', 'matrix', 'recsys', 'svd'], # arbitrary keywords
   classifiers = [],
+  install_requires = ['scipy','numpy'], # required packages
 )
